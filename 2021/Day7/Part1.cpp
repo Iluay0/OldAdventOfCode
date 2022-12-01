d236850e0a138192a68d6661671a8117b9facc4e
Iluay <iluay0001@gmail.com>
2022-12-01 03:09:41 +0100

    day 7

--- 2021/Day7/Part1.cpp
+++ 2021/Day7/Part1.cpp
@@ -3,5 +3,26 @@
 
 void Part1::Run(std::list<std::string> inputs)
 {
+	std::vector<int> startPositions;
+	for (auto& it : inputs)
+	{
+		auto positions = Utils::split(it, ',');
+		for (auto& it2 : positions)
+		{
+			startPositions.push_back(stoi(it2));
+		}
+	}
 
+	std::sort(startPositions.begin(), startPositions.end());
+	int median = startPositions[startPositions.size() / 2];
+
+	std::cout << "Going to position " << median << "\n";
+
+	int fuel = 0;
+	for (auto& it : startPositions)
+	{
+		fuel += abs(median - it);
+	}
+
+	std::cout << "Fuel consumed: " << fuel << "\n";
 }
--- 2021/Day7/Part2.cpp
+++ 2021/Day7/Part2.cpp
@@ -3,5 +3,31 @@
 
 void Part2::Run(std::list<std::string> inputs)
 {
+	std::vector<int> startPositions;
+	for (auto& it : inputs)
+	{
+		auto positions = Utils::split(it, ',');
+		for (auto& it2 : positions)
+		{
+			startPositions.push_back(stoi(it2));
+		}
+	}
 
+	int mean = 0;
+	for (auto& it : startPositions)
+	{
+		mean += it;
+	}
+	mean /= startPositions.size();
+
+	std::cout << "Going to position " << mean << "\n";
+
+	int fuel = 0;
+	for (auto& it : startPositions)
+	{
+		int move = abs(mean - it);
+		fuel += (int)(move * (move + 1) / 2);
+	}
+
+	std::cout << "Fuel consumed: " << fuel << "\n";
 }
