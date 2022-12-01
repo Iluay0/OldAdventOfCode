2e71ae6d01a0dac7d20dba37dd59c5d260b53392
Iluay <iluay0001@gmail.com>
2022-12-01 13:06:09 +0100

    2022 day 1

--- 2022/Day1/Part1.cpp
+++ 2022/Day1/Part1.cpp
@@ -3,5 +3,18 @@
 
 void Part1::Run(std::list<std::string> inputs)
 {
-
+	int mostCalories = 0;
+	int curCalories = 0;
+	for (auto& it : inputs)
+	{
+		if (it == "")
+		{
+			if (curCalories > mostCalories)
+				mostCalories = curCalories;
+			curCalories = 0;
+		}
+		else
+			curCalories += stoi(it);
+	}
+	std::cout << "Most Calories carried: " << mostCalories << "\n";
 }
--- 2022/Day1/Part2.cpp
+++ 2022/Day1/Part2.cpp
@@ -3,5 +3,35 @@
 
 void Part2::Run(std::list<std::string> inputs)
 {
+	std::list<int> mostCalories = { 0, 0, 0 };
+	int curCalories = 0;
+	for (auto& it : inputs)
+	{
+		if (it == "")
+		{
+			for (auto it = mostCalories.begin(); it != mostCalories.end(); it++)
+			{
+				if (curCalories > *it)
+				{
+					mostCalories.insert(it, curCalories);
+					break;
+				}
+			}
+			curCalories = 0;
+		}
+		else
+			curCalories += stoi(it);
 
+		if (mostCalories.size() > 3)
+			mostCalories.pop_back();
+	}
+
+	int count = 1;
+	for (auto& it : mostCalories)
+	{
+		std::cout << "Calories of top " << count << " : " << it << "\n";
+		count++;
+	}
+
+	std::cout << "Total: " << Utils::listSum(mostCalories);
 }
