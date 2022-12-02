7f2d63ec1e6911eeb3908ee2987bc76f9f460ff1
Iluay <iluay0001@gmail.com>
2022-12-02 13:38:33 +0100

    2022 day 2

--- 2022/Day2/Part1.cpp
+++ 2022/Day2/Part1.cpp
@@ -3,5 +3,19 @@
 
 void Part1::Run(std::list<std::string> inputs)
 {
+	int score = 0;
+	for (auto& it : inputs)
+	{
+		auto split = Utils::split(it, ' ');
+		char opponent = split[0][0];
+		char own = split[1][0];
+		opponent += 'Z' - 'C';
 
+		score += (own - 'X') + 1;
+		if (own == opponent)
+			score += 3;
+		else if (opponent == own - 1 || opponent == own + 2)
+			score += 6;
+	}
+	std::cout << "Final Score: " << score << "\n";
 }
--- 2022/Day2/Part2.cpp
+++ 2022/Day2/Part2.cpp
@@ -3,5 +3,33 @@
 
 void Part2::Run(std::list<std::string> inputs)
 {
+	int score = 0;
+	for (auto& it : inputs)
+	{
+		auto split = Utils::split(it, ' ');
+		char opponent = split[0][0];
+		char choice = split[1][0];
+		opponent += 'Z' - 'C';
 
+		char own = opponent;
+		if (choice == 'X')
+		{
+			own = opponent - 1;
+			if (own < 'X')
+				own += 3;
+		}
+		else if (choice == 'Z')
+		{
+			own = opponent + 1;
+			if (own > 'Z')
+				own -= 3;
+		}
+
+		score += (own - 'X') + 1;
+		if (own == opponent)
+			score += 3;
+		else if (opponent == own - 1 || opponent == own + 2)
+			score += 6;
+	}
+	std::cout << "Final Score: " << score << "\n";
 }
