f4be165e82f69e841e030b8af00666cf2e5b64ca
Iluay <iluay0001@gmail.com>
2022-12-03 18:23:27 +0100

    2022 day 3

--- 2022/Day3/Part2.cpp
+++ 2022/Day3/Part2.cpp
@@ -3,5 +3,47 @@
 
 void Part2::Run(std::list<std::string> inputs)
 {
+	int sum = 0;
+	std::vector<char> chars;
+	for (int i = 0; i <= inputs.size(); i++)
+	{
+		auto it = inputs.begin();
+		std::advance(it, i);
 
+		if (i % 3 == 0)
+		{
+			if (i != 0)
+			{
+				if (chars[0] >= 'A' && chars[0] <= 'Z')
+					sum += (chars[0] - 'A') + 27;
+				else if (chars[0] >= 'a' && chars[0] <= 'z')
+					sum += (chars[0] - 'a') + 1;
+			}
+
+			if (i >= inputs.size())
+				break;
+
+			chars.clear();
+
+			for (auto& it2 : *it)
+				chars.push_back(it2);
+		}
+		else
+		{
+			if (i >= inputs.size())
+				break;
+
+			std::vector<char> charsLeft;
+			for (auto& it2 : *it)
+			{
+				auto itChar = std::find(chars.begin(), chars.end(), it2);
+				if (itChar != chars.end())
+					charsLeft.push_back(*itChar);
+			}
+
+			chars = charsLeft;
+		}
+	}
+
+	std::cout << sum;
 }
--- Utils/Utils.h
+++ Utils/Utils.h
@@ -14,6 +14,7 @@
 #include <regex>
 #include <map>
 #include <deque>
+#include <set>
 #include <windows.h>
 
 class Utils
